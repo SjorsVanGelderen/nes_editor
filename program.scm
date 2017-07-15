f8d919a5572ff4a126ae86400d49675f8125943a
Sjors van Gelderen <sjors@openmailbox.org>
2017-07-15 11:49:01 +0200

    Making palette drawing loop better

--- program.scm
+++ program.scm
@@ -23,99 +23,8 @@ Copyright 2017, Sjors van Gelderen
 (define window_caption "NES Editor")
 (define window_x 'centered)
 (define window_y 'centered)
-(define window_width 1280)
-(define window_height 720)
-
-;; Colors
-(define black (sdl2:make-color 0 0 12))
-(define dark_gray (sdl2:make-color 80 100 91))
-(define gray (sdl2:make-color 202 216 203))
-(define red (sdl2:make-color 255 1 1))
-(define green (sdl2:make-color 0 200 0))
-(define purple (sdl2:make-color 150 0 150))
-(define blue (sdl2:make-color 0 0 200))
-
-(define palette_rgb_values
-  (list #(0 0 0)
-	#(0 0 0) ;; Not used?
-	#(0 0 0) ;; Not used?
-	#(3 56 63)
-	#(4 60 19)
-	#(3 59 4)
-	#(13 51 3)
-	#(52 40 3)
-	#(89 26 5)
-	#(112 16 13)
-	#(114 14 55)
-	#(95 18 97)
-	#(60 26 122)
-	#(21 35 125)
-	#(3 47 103)
-	#(101 101 101)
-	
-	#(0 0 0) ;; Not used?
-	#(0 0 0) ;; Not used?
-	#(0 0 0) ;; Not used?
-	#(0 114 125)
-	#(0 121 61)
-	#(7 119 4)
-	#(54 109 0)
-	#(109 92 0)
-	#(159 74 0)
-	#(189 60 48)
-	#(192 52 112)
-	#(167 54 169)
-	#(120 65 204)
-	#(64 81 208)
-	#(15 99 179)
-	#(174 174 174)
-	
-	#(0 0 0) ;; Not used?
-	#(0 0 0) ;; Not used?
-	#(78 78 78)
-	#(62 194 205)
-	#(60 201 140)
-	#(85 199 83)
-	#(133 188 47)
-	#(189 172 44)
-	#(239 154 73)
-	#(255 138 127)
-	#(255 131 192)
-	#(247 133 250)
-	#(200 144 255)
-	#(143 161 255)
-	#(93 179 255)
-	#(254 254 255)
-	
-	#(0 0 0) ;; Not used?
-	#(0 0 0) ;; Not used?
-	#(182 182 182)
-	#(175 229 234)
-	#(174 232 208)
-	#(185 232 184)
-	#(204 227 169)
-	#(228 220 168)
-	#(248 213 180)
-	#(255 207 202)
-	#(255 204 229)
-	#(251 205 253)
-	#(232 209 255)
-	#(209 216 255)
-	#(188 223 255)
-	#(254 254 255)))
-
-;; NES palette
-(define palette_colors
-  (let loop ((rgb_list palette_rgb_values)
-	     (color_list (list)))
-    (if (> (length rgb_list) 0)
-	(let ((rgb (car rgb_list)))
-	  (loop (cdr rgb_list)
-		(cons (sdl2:make-color (vector-ref rgb 0)
-				       (vector-ref rgb 1)
-				       (vector-ref rgb 2))
-		      color_list)))
-	color_list)))
+(define window_width 1024)
+(define window_height 768)
 
 ;; Initialize SDL
 (sdl2:set-main-ready!)
@@ -148,10 +57,10 @@ Copyright 2017, Sjors van Gelderen
 	  (poll-loop)))
     
     (sdl2:fill-rect! (sdl2:window-surface window) #f dark_gray)
-    (gui-draw-palette)
+    (gui-draw-palette 32 32)
     (sdl2:update-window-surface! window)
     
-    (sdl2:delay! 10)
+    (sdl2:delay! 10) ;; Throttle CPU
 
     (if (not (sdl2:quit-requested?))
 	(logic (+ theta 0.1)))))
